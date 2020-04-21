8fca8d8478455e62fb795d498feccc3e0f48eaeb
Maurikzio <m.morocho1196@gmail.com>
2020-04-21 21:40:29 +0300

    Updates

---
+++ precache-manifest.5cc5b6c2384c6ad1a7aca7d2ff73655a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97685486d031640247a98056ba9e0460",
+    "url": "/posting-app/index.html"
+  },
+  {
+    "revision": "4a3154e85169cf2763fc",
+    "url": "/posting-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f87c06b5e7991af4115a",
+    "url": "/posting-app/static/js/2.cdd3bcb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/posting-app/static/js/2.cdd3bcb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a3154e85169cf2763fc",
+    "url": "/posting-app/static/js/main.4d828280.chunk.js"
+  },
+  {
+    "revision": "9e2d29ffb4160dadc993",
+    "url": "/posting-app/static/js/runtime-main.8134fc55.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/posting-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
